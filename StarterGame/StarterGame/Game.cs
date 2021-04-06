173af95f992e695d86f7f211083e74c002313f77
Wormlord13 <81758751+Wormlord13@users.noreply.github.com>
2021-04-05 21:27:01 -0400

    Map and Exit update
    All rooms have been created and linked to Exits

--- StarterGame/StarterGame/Game.cs
+++ StarterGame/StarterGame/Game.cs
@@ -19,46 +19,77 @@ namespace StarterGame
 
         public Room CreateWorld()
         {
-            Room outside = new Room("outside the main entrance of the university");
-            Room cctparking = new Room("in the parking lot at CCT");
-            Room boulevard = new Room("on the boulevard");
-            Room universityParking = new Room("in the parking lot at University Hall");
-            Room parkingDeck = new Room("in the parking deck");
-            Room cct = new Room("in the CCT building");
-            Room theGreen = new Room("in the green in from of Schuster Center");
-            Room universityHall = new Room("in University Hall");
-            Room schuster = new Room("in the Schuster Center");
+            Room centralHallway = new Room("in the Central Hallway");
+            Room lobby = new Room("in the Lobby");
+            Room southWestHallway = new Room("in the Southwest Hallway");
+            Room studyHall = new Room("in the Study Hall");
+            Room library = new Room("in the Library");
+            Room broomCloset = new Room("in the Broom Closet");
+            Room hexLab = new Room("in the Hex Lab");
+            Room observatory = new Room("in the Observatory");
+            Room southEastHallway = new Room("in the Southeast Hallway");
+            Room hauntedBathroom = new Room("in the Haunted Bathroom");
+            Room teleportationStudio = new Room("in the Teleportation Studio");
+            Room garden = new Room("in the Garden");
+            Room northEastHallway = new Room("in the Northeast Hallway");
+            Room cafeteria = new Room("in the Cafeteria");
+            Room examHall = new Room("in the examHall");
+            Room carpentryShop = new Room("in the Carpentry Shop");
+            Room northWestHallway = new Room("in the Northwest Hallway");
+            Room potionsHall = new Room("in the Potions Hall");
+            Room teachersLounge = new Room("in the Teacher's Lounge");
+            Room necromancyStudio = new Room("in the Necromancy Studio");
+            Room gym = new Room("in the Gym");
 
-            outside.SetExit("west", boulevard);
+            centralHallway.SetExit("Garden", garden);
+            centralHallway.SetExit("Lobby", lobby);
 
-            boulevard.SetExit("east", outside);
-            boulevard.SetExit("south", cctparking);
-            boulevard.SetExit("west", theGreen);
-            boulevard.SetExit("north", universityParking);
+            garden.SetExit("Northeast Hallway", northEastHallway);
+            garden.SetExit("Northwest Hallway", northWestHallway);
+            garden.SetExit("Central Hallway", centralHallway);
+            
+            northWestHallway.SetExit("Necromancy Studio", necromancyStudio);
+            northWestHallway.SetExit("Gym", gym);
+            northWestHallway.SetExit("Teacher's Lounge", teachersLounge);
+            northWestHallway.SetExit("Potions Hall", potionsHall);
+            
+            northEasHallway.SetExit("Carpentry Shop", carpentryShop);
+            northEastHallway.SetExit("Cafeteria", cafeteria);
+            northEastHallway.SetExit("Exam Hall", examHall);
+            
+            lobby.SetExit("Southeast Hallway", southEastHallway);
+            lobby.SetExit("Southwest Hallway", southWestHallway);
+            lobby.SetExit("Central Hallway", centralHallway);
+            
+            southWestHallway.SetExit("Broom Closet", broomCloset);
+            southWestHallway.SetExit("Library", library);
+            southWestHallway.SetExit("Hex Lab", hexLab);
+            southWestHallway.SetExit("Observatory", observatory);
+            southWestHallway.SetExit("Study Hall", studyHall);
+            
+            southEastHallway.SetExit("Haunted Bathroom", hauntedBathroom);
+            southEastHallway.SetExit("Teleportation Studio", teleportationStudio);
+            
+            necromancyStudio.SetExit("Northwest Hallway", northWestHallway);
+            gym.SetExit("Northwest Hallway", northWestHallway);
+            teachersLounge.SetExit("Northwest Hallway", northWestHallway);
+            potionsHall.SetExit("Northwest Hallway", northWestHallway);
+            
+            cafeteria.SetExit("Northeast Hallway", northEastHallway);
+            examHall.SetExit("Northeast Hallway", northEastHallway);
+            carpentryShop.SetExit("Northeast Hallway", northEastHallway);
+            
+            broomCloset.SetExit("Southwest Hallway", southWestHallway);
+            library.SetExit("Southwest Hallway", southWestHallway);
+            hexLab.SetExit("Southwest Hallway", southWestHallway);
+            observatory.SetExit("Southwest Hallway", southWestHallway);
+            studyHall.SetExit("Southwest Hallway", southWestHallway);
+            
+            hauntedBathroom.SetExit("Southeast Hallway", southEastHallway);
+            teleportationStudio.SetExit("Southeast Hallway", southEastHallway);
+            
 
-            cctparking.SetExit("west", cct);
-            cctparking.SetExit("north", boulevard);
-
-            cct.SetExit("east", cctparking);
-            cct.SetExit("north", schuster);
-
-            schuster.SetExit("south", cct);
-            schuster.SetExit("north", universityHall);
-            schuster.SetExit("east", theGreen);
-
-            theGreen.SetExit("west", schuster);
-            theGreen.SetExit("east", boulevard);
-
-            universityHall.SetExit("south", schuster);
-            universityHall.SetExit("east", universityParking);
-
-            universityParking.SetExit("south", boulevard);
-            universityParking.SetExit("west", universityHall);
-            universityParking.SetExit("north", parkingDeck);
-
-            parkingDeck.SetExit("south", universityParking);
-
-            return outside;
+            return studyHall;
         }
 
         /**
@@ -77,7 +108,7 @@ namespace StarterGame
                 Command command = parser.ParseCommand(Console.ReadLine());
                 if (command == null)
                 {
-                    Console.WriteLine("I don't understand...");
+                    Console.WriteLine("I didn't understand that command");
                 }
                 else
                 {
@@ -101,12 +132,12 @@ namespace StarterGame
 
         public string Welcome()
         {
-            return "Welcome to the World of CSU!\n\n The World of CSU is a new, incredibly boring adventure game.\n\nType 'help' if you need help." + player.CurrentRoom.Description();
+            return "Your sorcery exams are today!\n\n You have to learn all five spells before you take the exam.\n\nType 'help' if you need help." + player.CurrentRoom.Description();
         }
 
         public string Goodbye()
         {
-            return "\nThank you for playing, Goodbye. \n";
+            return "\nThank you for playing! \n";
         }
 
     }
